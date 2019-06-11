d9bdc650a14e1a420994c3d27261b1f466f90d94
matkuh <matkuh@gmail.com>
2019-06-10 20:41:57 -0700

    add time conversion for next arrival and minutes away, append that info to the table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,72 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyB262t6Ap-dXTJNpsiPZSiM9UeHsE7b9ZM",
-    authDomain: "mattproject-3cf8d.firebaseapp.com",
-    databaseURL: "https://mattproject-3cf8d.firebaseio.com",
-    projectId: "mattproject-3cf8d",
-    storageBucket: "mattproject-3cf8d.appspot.com",
-    messagingSenderId: "957094050232",
-    appId: "1:957094050232:web:054f3e7c71d47912"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-  var trainName = "";
-  var destination = "";
-  var firstTrainTime= "";
-  var frequency = "";
-  
-  $("#submit-btn").on("click", function(event){
+    apiKey: "AIzaSyCxd7ARIkUcQXaknbqIvjqvKuwA4bu9ZLU",
+    authDomain: "sage-6dfab.firebaseapp.com",
+    databaseURL: "https://sage-6dfab.firebaseio.com",
+    projectId: "sage-6dfab",
+    storageBucket: "sage-6dfab.appspot.com",
+    messagingSenderId: "552074634211",
+    appId: "1:552074634211:web:5afb3e9166d61c5c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
+var nextTrain;
+
+
+$("#submit-btn").on("click", function (event) {
     event.preventDefault();
     trainName = $("#train-name").val().trim();
     destination = $("#train-dest").val().trim();
     firstTrainTime = $("#first-train").val().trim();
     frequency = $("#frequency-input").val().trim();
-      console.log(trainName);
-      console.log(destination);
-      console.log(firstTrainTime);
-      console.log(frequency);
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrainTime);
+    console.log(frequency);
     database.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency,
+
+
     });
 });
 
-database.ref().on("child_added", function(Childsnapshot){
-  $(".train-info").append("<tr><td>" +Childsnapshot.val().trainName +"</td><td>" +Childsnapshot.val().destination +"</td><td>"
-  +Childsnapshot.val().frequency +"</td><td></td><td>" + Childsnapshot.val().rateA +"</td></tr>");
-}, function(errorObject) {
-    console.log("Errors handled: " +errorObject.code);
+database.ref().on("child_added", function (Childsnapshot) {
+    var firstTimeConvert = moment(firstTrainTime, "HH:mm").subtract(1, "years")
+    var timeDif = moment().diff(moment(firstTimeConvert), "minutes")
+    var tRemainder = timeDif % frequency;
+    var timetillTrain = frequency - tRemainder;
+    var nextTrain = moment().add(timetillTrain, "minutes").format("HH:mm")
+    console.log(nextTrain)
+    var row = $("<tr>")
+    var trainTag = $("<th>")
+    trainTag.text(Childsnapshot.val().trainName)
+    var destTag = $("<th>")
+    destTag.text(Childsnapshot.val().destination)
+    var frequencyTag = $("<th>")
+    frequencyTag.text(Childsnapshot.val().frequency)
+    var arrivalTag = $("<th>")
+    arrivalTag.text(nextTrain)
+    var nextTag = $("<th>")
+    nextTag.text(timetillTrain)
+
+    row.append(trainTag)
+    row.append(destTag)
+    row.append(frequencyTag)
+    row.append(arrivalTag)
+    row.append(nextTag)
+
+    $(".train-info").append(row)
+
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
 });
+
